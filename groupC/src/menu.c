698478f9e3c85aeceba04feddb89a5dff7b36bd8
liu7920 <42726432+liu7920@users.noreply.github.com>
2018-11-12 13:41:29 +0900

    Update menu.c
    menu

--- groupC/src/menu.c
+++ groupC/src/menu.c
@@ -1,8 +1,10 @@
-#include <stdio.h>;
+#include <stdio.h>
 void menu(){
 
 	int input;
-
+	int log_check;
+	char str1[10];
+	char str2[20];
 	while(1){
 		printf("1.로그인 2.회원가입 3.종료\n");
 		printf("입력 : ");
@@ -10,6 +12,19 @@ void menu(){
 
 		if(input==1){
 			printf(">>로그인<<\n");
+			printf("아이디 입력 : ");
+			scanf("%s",str1);
+			printf("비밀번호 입력 : ");
+			scanf("%s",str2);
+			log_check=login(str1,str2);
+			if(log_check==1){
+				printf(">>로그인 완료<<\n");
+			}
+			else{
+				printf(">>로그인 실패<<\n");
+			}
+			//관리자 계정
+			//회원계정
 		}
 		else if(input==2){
 			printf(">>회원가입<<\n");

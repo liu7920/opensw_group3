9057786d578c7b464c1329d0e55c0f32b86b8842
liu7920 <42726432+liu7920@users.noreply.github.com>
2018-11-21 13:20:18 +0900

    Update login.c
    add

--- groupC/src/login.c
+++ groupC/src/login.c
@@ -2,12 +2,18 @@
 
 int login(char *str1, char *str2){
 
+	//str1 = 아이디
+	//str2 = 비밀번호
 
-	if(){//관리자 비교
-		return 1;
+	if(strcmp(str1,)==0){//관리자 비교
+		if(strcmp(str2,)==0){
+			return 1;
+		}
 	}
-	else if(){//회원 비교
-		return 2;
+	else if(strcmp(str2,)==0){//회원 비교
+		if(strcmp(str2,)==0){
+			return 2;
+		}
 	}
 
 	return 0;

a9ae63229777fe70b6d5edbec6a885b24c7032b3
liu7920 <42726432+liu7920@users.noreply.github.com>
2018-11-28 13:32:25 +0900

    Update admit.c
    db추가 할 것

--- groupC/src/admit.c
+++ groupC/src/admit.c
@@ -3,35 +3,79 @@
 void admit_menu(){
 
 	int i;
+	char str[20],_str[200];
 
 	while(1){
-		printf("1.공지사항 수정 2.메시지 보내기 3.회원 데이터 초기화 4.회원 삭제 5.관리자 비밀번호 변경 6.로그아웃\n");
+		printf("1.게시판 수정 2.회원 데이터 초기화 3.회원 삭제 4.관리자 비밀번호 변경 5.로그아웃\n");
 		printf("입력 : ");
 		scanf("%d",&i);
 		if(i==1){
-			printf(">>공지사항 수정<<\n");
+			while(1){
+				printf(">>게시판 수정<<\n");
+				// 보여주기 예시
+				// 1 ~~~~~~~~~~~
+				// 2 ***********
+				printf("1.추가 2.삭제 3.취소\n");
+				scanf("%d",&i);
+				if(i==1){
+					printf("추가 게시판 내용 입력 : ");
+					scanf("%s",_str);
+					// _str을 게시판에 추가 추가할때 넘버를 따로 줘야됨
+				}
+				else if(i==2){
+					printf("삭제할 게시판 번호 입력 : ");
+					scanf("%d",&i);
+					//번호에 해당하는 게시판 삭제
+				}
+				else if(i==3){
+					printf("취소\n");
+					break;
+				}
+				else{
+					printf("잘못된 입력\n");
+				}
+			}
 		}
 		else if(i==2){
-			printf(">>메시지 보내기<<\n");
-		}
-		else if(i==3){
 			printf(">>회원 데이터 초기화<<\n");
 		}
-		else if(i==4){
+		else if(i==3){
 			printf(">>회원 삭제<<\n");
+			printf("회원 아이디 입력 : ");
+			scanf("%s",str);
+			if(/*str == id(db)*/){
+				printf("1.삭제  2.취소");
+				printf("입력 : ");
+				scanf("%d",&i)
+				if(i==1){
+					printf("삭제 성공\n");
+					//db에서 회원 삭제
+				}
+				else if(i==2){
+					printf("삭제 취소\n");
+				}
+				else{
+					printf("잘못된 입력\n");
+				}
+			}
+		}
+		else if(i==4){
+			printf(">>관리자 비밀번호 변경<<\n");
+			printf("비밀번호 입력 : ");
+			scanf("%s",str);
+			if(/*str == pw(db)*/){
+				printf("새로운 비밀번호 입력 : ");
+				scanf("%s",str);
+				//str을 새로운 비밀번호로 db에 넣기
+				printf("비밀번호 변경 완료\n");
+			}
+			else{ //틀리다면
+				printf("입력 비밀번호가 틀립니다.\n");
+			}
 		}
 		else if(i==5){
-			printf(">>관리자 비밀번호 변경<<\n");
-		}
-		else if(i==6){
 			printf(">>로그아웃<<\n");
 			break;
 		}
-		//관지라 비밀번호 변경
-		//회원 삭제
-		//공지사항 수정
-		//메시지
-		//회원 데이터 초기화
-		//로그아웃
 	}
 }
